a560e40733a0733f2204e7e6df7625ebb31d9e40
Andres <andreshurdato16@gmail.com>
2026-09-23 10:08:15 -0500

    Appjs Completo

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Provider as PaperProvider} from 'react-native-paper';
+
+import MisAhorros from './screens/MisAhorros';
+import TodosAhorros from './screens/TodosAhorros';
+
+const Drawer = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola Fondos</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="MisAhorros">
+          <Drawer.Screen name="MisAhorros" component={MisAhorros} />
+          <Drawer.Screen name="TodosAhorros" component={TodosAhorros} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
